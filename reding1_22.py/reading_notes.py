9257c4ce4f259c33f1b4cd43cc0512998125f131
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-22 15:22:58 +0900

    何も参照せずにコード読んだ

--- reding1_22.py/reading_notes.py
+++ reding1_22.py/reading_notes.py
@@ -1,22 +1,28 @@
 import json
+# jsonファイルが読めるようにインポート
 
 def main():
     with open("data.json", encoding="UTF-8") as f:
         data = json.load(f)
+# 文字コードUTF-8でjsonファイル読むよ、名前はf
 
     result = {}
+# jsonファイルのデータを引数無しでresultにセット
 
     for item in data:
         if item["category"] not in result:
             result[item["category"]] = []
+# if not がどうなってるの？
 
         if item["price"] >= 1000:
             result[item["category"]].append(item["name"])
+# 1000円以上のやつはTRUE、カテゴリーと名前で表示できるようにしておく
 
     for k in result:
         print(k)
         for v in result[k]:
             print(" - " + v)
+# jsonファイルの中身表示と、-つけてkの中身全部表示して
 
 if __name__ == "__main__":
     main()
